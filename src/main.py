29890206f6c948e38077e1fe966adca3b4f312df
Jordan <jordansilva102@gmail.com>
2026-04-21 19:40:26 -0300

    improving sync of files

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
+import os
 import shutil
 import sys
-from pathlib import Path
 
+# from pathlib import Path
 from generate_page import generate_pages_recursive
 
 STATIC = "./static"
@@ -16,27 +17,22 @@ def main():
     if len(sys.argv) > 1:
         base_path = sys.argv[1]
 
-    sync_folder(STATIC, PUBLIC)
+    sync_folder_recursive(STATIC, PUBLIC)
     generate_pages_recursive(CONTENT, TEMPLATE, PUBLIC, base_path)
 
 
-def sync_folder(source: str | Path, destination: str | Path) -> None:
-    source = Path(source)
-    destination = Path(destination)
+def sync_folder_recursive(source_dir_path, dest_dir_path):
+    if not os.path.exists(dest_dir_path):
+        os.mkdir(dest_dir_path)
 
-    if not source.exists():
-        raise FileNotFoundError(f"Pasta de origem não encontrada: {source}")
-
-    if not source.is_dir():
-        raise NotADirectoryError(f"Origem não é uma pasta: {source}")
-
-    # Limpa o destino (ou cria se não existir)
-    if destination.exists():
-        shutil.rmtree(destination)
-    destination.mkdir(parents=True)
-
-    # Copia tudo da origem para o destino
-    shutil.copytree(source, destination, dirs_exist_ok=True)
+    for filename in os.listdir(source_dir_path):
+        from_path = os.path.join(source_dir_path, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        print(f" * {from_path} -> {dest_path}")
+        if os.path.isfile(from_path):
+            shutil.copy(from_path, dest_path)
+        else:
+            sync_folder_recursive(from_path, dest_path)
 
 
 if __name__ == "__main__":
